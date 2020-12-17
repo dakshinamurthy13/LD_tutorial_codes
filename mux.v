6f2f448d44fbc4c4bc894c885961745304fbca3d
dakshinamurthy <dakshinamurthy13@gmail.com>
2020-12-17 22:39:21 +0530

    4-1 Mux using Structural Model

--- mux.v
+++ mux.v
@@ -1,4 +1,4 @@
-module mux4to1(output y,input d0,d1,d2,d3,a,b);
+module mux4to1(output y,input en,d0,d1,d2,d3,a,b);
 	wire and1,and2,and3,and4;
 	and a1(and1,d0,~a,~b);
 	and a2(and2,d1,~a,b);
@@ -7,13 +7,14 @@ module mux4to1(output y,input d0,d1,d2,d3,a,b);
 	or o(y,and1,and2,and3,and4);
 endmodule
 module mux_tb;
-	reg d0,d1,d2,d3,a,b;
+	reg d0,d1,d2,d3,a,b,en;
 	wire y;
-	mux4to1 mux(y,d0,d1,d2,d3,a,b);
+	mux4to1 mux(y,en,d0,d1,d2,d3,a,b);
 	initial
 	begin
 		$dumpfile("mux_test.vcd");
 		$dumpvars(0,mux_tb);
+		en=1;
 		d0=1'b0;d1=1'b0;d2=1'b0;d3=1'b0;a=1'b0;b=1'b0;
 			#10;
 		d0=1'b1;d1=1'b0;d2=1'b0;d3=1'b0;a=1'b0;b=1'b0;
